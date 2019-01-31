5eff1088ab63b0763588b60a3f560eaa9e5cfb98
Tom Bingham <tominthetrees@gmail.com>
2019-01-31 14:57:18 -0800

    Added cat tower toy.

--- cat_facts.py
+++ cat_facts.py
@@ -6,4 +6,4 @@ cat_facts = {'Tippi':(6, 'White', 'Female'),
              'Kse':(5, 'Black and Brown', 'Female'),
              'Lilee':(1, 'Black, Gray, and Rust', 'Female')}
 
-cat_toys = ['mouse', 'birdy', 'tiger']
+cat_toys = ['mouse', 'birdy', 'tiger', 'cat tower']
