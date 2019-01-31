e9b9480bfc5687dd47f8ee5aa1e5176805866bbe
Tom Bingham <tominthetrees@gmail.com>
2019-01-31 14:51:06 -0800

    Added cat toys.

--- cat_facts.py
+++ cat_facts.py
@@ -5,3 +5,5 @@ A dictionary of Cat Facts
 cat_facts = {'Tippi':(6, 'White', 'Female'),
              'Kse':(5, 'Black and Brown', 'Female'),
              'Lilee':(1, 'Black, Gray, and Rust', 'Female')}
+
+cat_toys = ['mouse', 'birdy', 'tiger']
