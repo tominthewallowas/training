2d293ede3029869d93fa35e508e610c9446b7c02
tombo <tominthetrees@gmail.com>
2019-08-10 10:46:15 -0700

    Corrected spelling.

--- cat_facts.py
+++ cat_facts.py
@@ -2,8 +2,8 @@
 A dictionary of Cat Facts
 '''
 
-cat_facts = {'Tippi':(6, 'White', 'Female'),
-             'Kse':(5, 'Black and Brown', 'Female'),
-             'Lilee':(1, 'Black, Gray, and Rust', 'Female')}
+cat_facts = {'Tippi': (6, 'White', 'Female'),
+             'Kse': (5, 'Black and Brown', 'Female'),
+             'Lilly': (1, 'Black, Gray, and Rust', 'Female')}
 
 cat_toys = ['mouse', 'birdy', 'tiger', 'cat tower']
